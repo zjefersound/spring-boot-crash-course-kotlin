8dd49c914585c7f73d0253f44420a2b826291c79
zjefersound <jeferson.2017317264@gmail.com>
2026-02-12 23:39:15 -0300

    Field validation

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,7 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("tools.jackson.module:jackson-module-kotlin")
 	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
--- src/main/kotlin/com/zjefersound/spring_boot_crash_course_kotlin/BillDTO.kt
+++ src/main/kotlin/com/zjefersound/spring_boot_crash_course_kotlin/BillDTO.kt
@@ -1,7 +1,13 @@
 package com.zjefersound.spring_boot_crash_course_kotlin
 
+import org.hibernate.validator.constraints.Length
+
 data class BillDTO (
+
     val id: Long,
+
+    @field:Length(min = 3, max = 255, message = "Bill ID must be between 3 and 255")
     val description: String,
+
     val amount: Long
 )
